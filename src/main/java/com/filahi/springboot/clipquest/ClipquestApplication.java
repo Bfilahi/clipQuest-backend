3237b871ec8adcd5f6295c5d691e6730cfa7b8ce
Bfilahi <abdelbassete.filahi@gmail.com>
2025-10-16 01:35:00 +0200

    First commit

---
+++ src/main/java/com/filahi/springboot/clipquest/ClipquestApplication.java
@@ -0,0 +1,13 @@
+package com.filahi.springboot.clipquest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClipquestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClipquestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/filahi/springboot/clipquest/ClipquestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.filahi.springboot.clipquest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClipquestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
